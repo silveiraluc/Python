880fbf910ec0abd5ecee1bb2a78efbcacd9a453c
silveiraluc <lucas.silveira@hotmail.es>
2022-11-10 18:04:49 -0300

    Create funciones.py

---
+++ funciones.py
@@ -0,0 +1,17 @@
+import os
+os.system("cls")
+
+def repetir():
+    print (5+5)
+    print (10/2)
+    print (6+6)
+
+print ('Aqui comienza mi otro programa')
+
+repetir()
+
+print ('Aqui hago otra cosa')
+
+repetir()
+
+print ('Aqui finalizo mi programa')
